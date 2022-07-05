34b6c035a48220d8d888712f6ef6add1a8327f34
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-07-04 21:21:57 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import { ThemeProvider } from 'next-themes'
 
 import '../styles/main.css'
 
@@ -21,7 +22,7 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider><Component {...pageProps} /><ThemeProvider/>
     </>
   )
 }
